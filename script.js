521b3d3c0591303706340ee3a0841f599a81d686
Paulo Augusto <paulo-augusto12@outlook.com>
2022-09-29 20:06:55 -0300

    Clone tela de login league of legends finalizado

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-const userNameInput = document.querySelector('.username')
-const passwordInput = document.querySelector('.password')
+const inputs = document.querySelectorAll('.input')
+const button = document.querySelector('.loginButton')
+
 
 const handleFocus = ({ target }) => {
     const span = target.previousElementSibling
     span.classList.add('spanActive')
 }
-userNameInput.addEventListener('focus' ,  handleFocus)
 
-passwordInput.addEventListener('click' , ()=> console.log('clicou em senha'))
+const offFocus = ({ target }) => {
+
+    if(target.value === ''){
+        const span = target.previousElementSibling
+        span.classList.remove('spanActive')
+    }
+    
+}
+
+const handleChange = () => {
+    const [username , password] = inputs
+
+    if( username.value && password.value.length >=8 ){
+        
+        button.removeAttribute('disabled')
+    }else{
+     button.setAttribute('disabled' , '')   
+    }
+}
+
+inputs.forEach((input)=> input.addEventListener('focus', handleFocus));
+inputs.forEach((input)=> input.addEventListener('focusout', offFocus));
+
+inputs.forEach((input)=> input.addEventListener('input', handleChange));
